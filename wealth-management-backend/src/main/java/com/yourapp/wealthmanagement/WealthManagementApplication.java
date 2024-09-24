014096f0f8d384f448e86f9b3321cfca06d24fce
psankesh9 <69172877+psankesh9@users.noreply.github.com>
2024-09-24 11:55:51 -0700

    Update WealthManagementApplication.java

---
+++ wealth-management-backend/src/main/java/com/yourapp/wealthmanagement/WealthManagementApplication.java
@@ -0,0 +1,11 @@
+package com.yourapp.wealthmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WealthManagementApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WealthManagementApplication.class, args);
+    }
+}
