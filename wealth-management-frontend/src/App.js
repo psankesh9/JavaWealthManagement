29b2b1295bd57ac4574bcfac2c6dca54ee158d54
psankesh9 <69172877+psankesh9@users.noreply.github.com>
2024-09-24 12:28:15 -0700

    Update App.js

---
+++ wealth-management-frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import UserList from './components/UserList';
+import AddUser from './components/AddUser';
+
+function App() {
+    return (
+        <div>
+            <AddUser />
+            <UserList />
+        </div>
+    );
+}
+
+export default App;
